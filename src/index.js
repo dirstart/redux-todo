605279296fb71c87a59f57c2fdc2fc3aebb59bd5
dirstart <835041058@qq.com>
2018-06-14 17:06:01 +0800

    redux is easy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import store from './redux/store';
+
 import './reset.css';
 import './app.less';
 
---
+++ src/redux/reducer.js
@@ -0,0 +1,36 @@
+/* 
+    万事万物都有规则。
+    我们努力了，行动了，却未必会成功。
+
+    让我们来制造规则吧。让 actions 不被白费。
+*/
+import {GROW, MIRACLE, PEOPLETAG} from './actions';
+import {makeMiracle, startGrow} from './actions';
+
+// 人生的一开始，是一张白纸
+const whitePaper = {
+    girlFriend: [],
+    miracle: [],
+    identity: PEOPLETAG.child
+};
+
+// 于是我们人生的旅途开始了
+export const YourLife = (state = whitePaper, action) => {
+    switch (action.type) {
+        case GROW:
+            return Object.assign({}, state, {
+                identity: action.growType
+            });
+        case MIRACLE:
+            return Object.assign({}, state, {
+                miracle: [
+                    ...state.miracle,
+                    {
+                        text: action.text
+                    }
+                ]
+            });
+        default: 
+            return state;
+    }
+};
